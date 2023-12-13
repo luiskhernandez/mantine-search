b857968767d4283e72df7c8837674848c6399bd3
Luis C Hernández <luis.macias@koombea.com>
2023-12-13 16:34:05 -0500

    Add input number

--- components/DataTable/dataTable.tsx
+++ components/DataTable/dataTable.tsx
@@ -1,6 +1,6 @@
 // @ts-nocheck
 import { DataTable } from 'mantine-datatable';
-import { Checkbox, Container, Flex, Input, Select } from '@mantine/core';
+import { Checkbox, Container, Flex, Input, Select, NumberInput } from '@mantine/core';
 import { useState } from 'react';
 
 const PAGE_SIZES = [5, 10, 15, 20];
@@ -32,32 +32,44 @@ export default function ResultTable({
         },
         {
           accessor: 'frequency',
-          render: (row) => (<Select
-            style={{ flex: '1 1 0%;' }}
-            label="Option"
-            value={row.frequency}
-            data={['A', 'B', 'C', 'D', 'E']}
-            placeholder="Select"
-            onChange={(value) => {
-               console.log('cambiar a ', value, row);
+          render: (row) => (
+            <Select
+              style={{ flex: '1 1 0%;' }}
+              label="Option"
+              value={row.frequency}
+              data={['A', 'B', 'C', 'D', 'E']}
+              placeholder="Select"
+              onChange={(value) => {
+                console.log('cambiar a ', value, row);
               }}
-          />),
+            />
+          ),
         },
         {
           accessor: 'add to iv?',
           render: (row) => {
             const [state, setState] = useState(false);
             return (
-            <Container w={300}>
-              <Flex gap={8} align="center">
-                <Checkbox value={state} onChange={() => { setState(!state); }} />
-                {state && <Input onChange={(value) => {
-               console.log('cambiar a ', value, row);
-              }}
-                />}
-              </Flex>
-            </Container>
-          );
+              <Container w={200}>
+                <Flex gap={8} align="center">
+                  <Checkbox
+                    value={state}
+                    onChange={() => {
+                      setState(!state);
+                    }}
+                  />
+                  {state && (
+                      <NumberInput
+                        width={30}
+                        variant="filled"
+                        onChange={(value) => {
+                          console.log('cambiar a ', value, row);
+                        }}
+                      />
+                  )}
+                </Flex>
+              </Container>
+            );
           },
         },
       ]}
